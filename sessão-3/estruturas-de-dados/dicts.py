014bda339c6d832a409b73d90efee133a60416ef
professordiogodev <diogo@dionamite.com>
2026-03-17 15:03:36 +0000

    adicionar exercícios nos dicts

--- sessão-3/estruturas-de-dados/dicts.py
+++ sessão-3/estruturas-de-dados/dicts.py
@@ -6,3 +6,9 @@ cadeira = {
 }
 
 print(f"Esta cadeira tem { cadeira["pernas"] } perninhas.")
+
+# Exercício:
+# 1. Adicionar 3 propriedades à cadeira (com valores à escolha):
+# - Altura, Largura, Comprimento
+
+# 2. Calcular o volume da cadeira
